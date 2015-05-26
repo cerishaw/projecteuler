d7f7776c291fb47942f12d913394b17deac86bbd
Ceri Shaw <ceri@freeagent.com>
2015-05-26 21:02:12 +0100

    Stubs for 23 and 24 - not solved yet

---
+++ problem24.rb
@@ -0,0 +1,14 @@
+def permutate(array)
+  permutations = []
+  num_perms = a
+end
+
+def perms(n)
+  n.downto(1).inject(:*)
+end
+
+def number_at_variation(array, variation)
+  digit1 = array[variation / perms(array.count-1)]
+  remainder = array - [digit1]
+  
+end

9045c25db3460ea69ec6fb3f36951e0a2738b410
Ceri Shaw <ceri@freeagent.com>
2015-05-28 20:24:03 +0100

    solve problem 24

--- problem24.rb
+++ problem24.rb
@@ -1,14 +1,23 @@
-def permutate(array)
-  permutations = []
-  num_perms = a
-end
-
 def perms(n)
   n.downto(1).inject(:*)
 end
 
 def number_at_variation(array, variation)
-  digit1 = array[variation / perms(array.count-1)]
-  remainder = array - [digit1]
+  answer = []
+  remainder = variation - 1
+  iterations = array.count - 1
+
+  i = 0
+  while i < iterations
+    perms = perms(array.count - 1)
+    answer << array[remainder / perms]
+    remainder = remainder - array.index(answer[i]) * perms
+    array.delete(answer[i])
+    i = i + 1
+  end
   
+  answer << array[0]
+  answer.join
 end
+
+number_at_variation [0,1,2,3,4,5,6,7,8,9], 1000000

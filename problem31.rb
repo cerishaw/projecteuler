631bf7a5d9bfed9833550cd2bd091987d83544e8
Ceri Shaw <ceri@freeagent.com>
2015-06-02 19:44:15 +0100

    start of problem 31

---
+++ problem31.rb
@@ -0,0 +1,16 @@
+def coin_count
+  variations = []
+  coins = [100, 50, 20, 10, 5, 2, 1]
+  max = 200 / 100
+  i = max
+  while i <= 0
+    make_up = []
+    sum = i * coins[0]
+    make_up << i
+    
+  end
+end
+
+def make_total(total, array)
+  
+end
